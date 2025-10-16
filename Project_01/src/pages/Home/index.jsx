8e93a72fe674653fafadabd3d1184fc502bba9de
ryanbrittes <ryanbrittes027@gmail.com>
2025-10-15 21:01:37 -0400

    [ADD] - Adicionando repositório

---
+++ Project_01/src/pages/Home/index.jsx
@@ -0,0 +1,36 @@
+import './style.css'
+import api from '../../services/api'
+import { useState, useRef } from 'react'
+
+function Home(){
+
+    let [output, setOutput] = useState([])
+
+    const inputMessage = useRef()
+
+    async function getOutput(){
+        console.log(output["data"])
+    }   
+
+    async function postInput(){
+        output = await api.post('/input', {
+            input: inputMessage.current.value
+        })
+
+        setOutput(output)
+        getOutput()
+    }
+
+    return (
+        <div id="main-container">
+            <span id='chat-content'>
+                <h1>Assistente Virtual</h1>
+                {<span>{output['data']}</span>}
+                <input type="text" name="input-user" placeholder="Digite alguma coisa aqui" ref={inputMessage}/>
+                <button type="button" onClick={postInput}>Enviar</button>
+            </span>
+        </div>
+    )
+}
+
+export default Home
