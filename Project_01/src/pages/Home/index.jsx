8b59551bcff71bfe2b54f00b2e9b8dad9beb5aff
ryanbrittes <87885748+RyanBrittes@users.noreply.github.com>
2025-10-16 18:37:38 +0000

    [EDIT] - Pasta assets com icone adicionados e caminho para adicionar à estrutura html também

--- Project_01/src/pages/Home/index.jsx
+++ Project_01/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import './style.css'
 import api from '../../services/api'
 import { useState, useRef } from 'react'
+import SendIcon from "../../assets/send_icon.png"
 
 function Home(){
 
@@ -22,11 +23,11 @@ function Home(){
     }
 
     return (
-        <div id="main-container">
+        <div id="main-container"> 
             <span id='chat-content'>
                 <h1>Assistente Virtual</h1>
                 {<span>{output['data']}</span>}
-                <input type="text" name="input-user" placeholder="Digite alguma coisa aqui" ref={inputMessage}/>
+                <input autofocus type="text" name="input-user" placeholder="Digite alguma coisa aqui" ref={inputMessage}/>
                 <button type="button" onClick={postInput}>Enviar</button>
             </span>
         </div>
