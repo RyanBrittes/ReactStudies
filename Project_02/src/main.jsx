98598ea43e7901cac5206216cde1634a56fff941
ryanbrittes <87885748+RyanBrittes@users.noreply.github.com>
2025-10-22 19:55:49 +0000

    [EDIT] - Arquivos editados para estilização

--- Project_02/src/main.jsx
+++ Project_02/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import MainScreen from './pages/MainScreen'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MainScreen />
   </StrictMode>,
 )
--- Project_02/src/pages/MainScreen/index.jsx
+++ Project_02/src/pages/MainScreen/index.jsx
@@ -1,20 +1,31 @@
 import './style.css'
+import RightArrow from '../../assets/right-arrow.png'
 
 function MainScreen() {
 
-
     return (
         <div id="main-container">
-            <div id="chat">
+            <div id="chat-container">
                 <p>Texto de output</p>
                 <h2>Como posso lhe ajudar hoje?</h2>
                 <form>
-                    <input type="text" placeholder='Envie uma mensagem...' />
-                    <button type="button"></button>
-                    
+                    <input autoFocus type="text" placeholder='Envie uma mensagem...' />
+                    <button type="button">
+                        <img src={RightArrow} alt="Send Button" />
+                    </button>
+                    <div id="additional-buttons">
+                        <button type="button">
+                            <img src={RightArrow} alt="Send Button" />
+                        </button>
+                        <button type="button">
+                            <img src={RightArrow} alt="Send Button" />
+                        </button>
+                    </div>
                 </form>
             </div>
 
         </div>
     )
 }
+
+export default MainScreen
