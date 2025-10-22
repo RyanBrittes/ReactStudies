54af1c8e22c66a565198d07737a289397dc34b1d
ryanbrittes <87885748+RyanBrittes@users.noreply.github.com>
2025-10-22 16:43:42 +0000

    [ADD] - Pastas MainScreen e SideFlap adicionadas junto com seus repositórios de index.jsx e style.css

---
+++ Project_02/src/pages/MainScreen/index.jsx
@@ -0,0 +1,20 @@
+import './style.css'
+
+function MainScreen() {
+
+
+    return (
+        <div id="main-container">
+            <div id="chat">
+                <p>Texto de output</p>
+                <h2>Como posso lhe ajudar hoje?</h2>
+                <form>
+                    <input type="text" placeholder='Envie uma mensagem...' />
+                    <button type="button"></button>
+                    
+                </form>
+            </div>
+
+        </div>
+    )
+}
